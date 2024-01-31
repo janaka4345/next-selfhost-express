19305b5de4c40348c2fc8538ca05b1397168f85a
banuka <banukaramanayaka76@gmail.com>
2024-01-31 22:08:03 +0530

    other:font loading

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
-import { Inter, Lusitana } from "next/font/google";
+import { Inter, Lusitana, Roboto } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({ subsets: ["latin"], weight: ['400', '700'] })
 const lusitana = Lusitana({ subsets: ["latin"], weight: ['400', '700'] })
 
 export const metadata = {
@@ -12,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <p className={lusitana.className}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ad et quasi beatae doloremque optio voluptates qui blanditiis eligendi ipsam deserunt, hic veritatis saepe tempore quas ipsa, pariatur similique debitis.
           Illum possimus delectus quasi, voluptatibus repudiandae maxime tenetur ex laborum voluptate necessitatibus. Corrupti dicta, porro voluptatibus soluta mollitia amet, reprehenderit dignissimos expedita excepturi, nemo iste. Inventore accusamus aperiam dolore veritatis.
           Consectetur porro aliquid laborum deleniti minus ipsum, dolores quibusdam perferendis facilis nobis assumenda libero odit sit doloribus adipisci repellat quidem quo reiciendis distinctio optio accusantium harum iusto animi. Dolorem, tempora.
